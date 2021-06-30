9663a0cd7862474b14acb634fc7eebee5a7e0579
Christiano0407 <placebo0704@gmail.com>
2021-06-30 08:19:53 -0500

    image-gridFinished

--- src/js/appCss.js
+++ src/js/appCss.js
@@ -1,1 +1,15 @@
 console.log("Grid"); 
+console.group("Image")
+
+let images = document.querySelectorAll(`.image`);
+console.log(images);
+
+window.addEventListener(`DOMContentLoaded`, (e) => {
+    images.forEach((image, idx) => {
+        setTimeout(() => {
+            image.classList.add(`active`); 
+        }, idx * 100); 
+    })
+})
+
+console.groupEnd(); 
